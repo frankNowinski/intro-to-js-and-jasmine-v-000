76af70f888df3e83ea4c52cd8881b1c2f737da00
frankNowinski <nowinski.frank@gmail.com>
2016-02-02 14:11:20 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(ice_cream) {
+    return 'I love ' + ice_cream;
+}
+
+function shouting(string) {
+    return string.toUpperCase();
+}
+
+function roundDown(number) {
+    return Math.floor(number);
+}
+
+function theTruth() {
+    return 1 + 1 == 2
+}
